8b77ac9c2ad222ed26298bc5172926a9cd35ad82
Will Wylie <wyliew@umich.edu>
2024-08-10 21:38:36 -0400

    Added some necessary functions for database handler

--- flashlearn/utils/database.py
+++ flashlearn/utils/database.py
@@ -1,6 +1,8 @@
 import sqlite3
 import threading
 
+from typing import Optional
+
 class DatabaseManager:
     _instance = None
 
@@ -14,7 +16,7 @@ class DatabaseManager:
     def __init__(self):
         self.create_tables()
 
-    def execute_query(self, query, params=None) -> list:
+    def execute_query(self, query: str, params: Optional[str]) -> list:
         with self.lock:  # Acquire lock
             cursor = self.connection.cursor()
             if params:
@@ -73,8 +75,8 @@ class DatabaseManager:
     def close_connection(self):
         self.connection.close()
 
-    def insert_into_table(self, table_name, **kwargs) -> None:
-        columns = ', '.join(kwargs.keys())
+    def insert_into_table(self, table_name: str, **kwargs) -> None:
+        columns = ', '.join(str(x) for x in kwargs.keys())
         placeholders = ', '.join('?' * len(kwargs))
         query = f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders})"
         self.execute_query(query, tuple(kwargs.values()))
@@ -95,7 +97,28 @@ class DatabaseManager:
             return self.execute_query(query, tuple(kwargs.values()))
         else:
             return self.execute_query(query)
+        
+    def union_select(self, table1: str, table2: str, *columns, **kwargs) -> list:
+        '''
+        Sample usage:
+          ```cards = union_select("SET", "SUPERSET", "title", user_id=1)```
+        this will return all titles of the rows in the SET and SUPERSET tables where user_id is 1'''
+        query = f"SELECT {', '.join(columns)} FROM {table1} UNION SELECT {', '.join(columns)} FROM {table2}"
+        if kwargs:
+            conditions = ' AND '.join(f"{key} = ?" for key in kwargs.keys())
+            query += f" WHERE {conditions}"
+            return self.execute_query(query, tuple(kwargs.values()))
+        else:
+            return self.execute_query(query)
+        
+    def update_table(self, table_name: str, set_dict: dict, where_dict: dict) -> list:
+        """set_dict = {'column_name': 'new_value'}
+           where_dict = {'column_name': 'value'}"""
+        set_columns = ', '.join(f"{key} = ?" for key in set_dict.keys())
+        where_columns = ' AND '.join(f"{key} = ?" for key in where_dict.keys())
+        query = f"UPDATE {table_name} SET {set_columns} WHERE {where_columns}"
+        return self.execute_query(query, tuple(set_dict.values()) + tuple(where_dict.values()))
     
-    def remove_from_table(self, table_name, **kwargs) -> list:
+    def remove_from_table(self, table_name: str, **kwargs) -> list:
         query = f"DELETE FROM {table_name} WHERE {list(kwargs.keys())[0]} = ?"
         return self.execute_query(query, tuple(kwargs.values()))
